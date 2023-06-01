36b8db130f41a9a41f55083f16dda5e0223696a3
nagibator99 <daniel.nikitin@tptlive.ee>
2023-06-02 00:06:24 +0300

    agario_new_ver

---
+++ AgarIO_Tech_with_Tim/client.py
@@ -0,0 +1,43 @@
+import socket
+import _pickle as pickle
+
+
+class Network:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #self.client.settimeout(10.0)
+        self.host = "localhost"
+        self.port = 10000
+        self.addr = (self.host, self.port)
+
+    def connect(self, name):
+        self.client.connect(self.addr)
+        self.client.send(str.encode(name))
+        val = self.client.recv(8)
+        return int(val.decode()) # can be int because will be an int id
+
+    def disconnect(self):
+        self.client.close()
+
+    def send(self, data, pick=False):
+        """
+        :param data: str
+        :param pick: boolean if should pickle or not
+        :return: str
+        """
+        try:
+            if pick:
+                self.client.send(pickle.dumps(data))
+            else:
+                self.client.send(str.encode(data))
+            reply = self.client.recv(2048*4)
+            try:
+                reply = pickle.loads(reply)
+            except Exception as e:
+                print(e)
+            return reply
+        except socket.error as e:
+            print(e)
+
+
+
