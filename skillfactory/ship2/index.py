6a96cc34c44475d09d6717ff23d7cba8f4221acc
Daniel Nikitin <90585324+DanielNikitin@users.noreply.github.com>
2023-09-01 14:28:48 +0300

    battle ship New V2
    issue with COLUMN empty insert need to fix

--- skillfactory/ship2/index.py
+++ skillfactory/ship2/index.py
@@ -1,31 +1,55 @@
 import random
 
+class Config:
+    def __init__(self):
+        self.SHOW_ENEMY_BOARD = 0  # Отображение игрового поля противника
+        self.POS_FREE_MESSAGE = 0  # Свободно-ли место возле корабля
+        self.CHOOSE_ORIENTATION = 1  # Выбор ориентации
+
+
 class BattleshipGame:
     def __init__(self):
-        self.LENGTH_OF_SHIPS = [1, 1, 1, 1]
-        self.PLAYER_BOARD = [[" "] * 6 for _ in range(6)]
-        self.COMPUTER_BOARD = [[" "] * 6 for _ in range(6)]
-        self.PLAYER_GUESS_BOARD = [[" "] * 6 for _ in range(6)]
-        self.COMPUTER_GUESS_BOARD = [[" "] * 6 for _ in range(6)]
-        self.LETTERS_TO_NUMBERS = {'1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5}
+        self.LENGTH_OF_SHIPS = [3]
+        self.BOARD_SIZE = 6
+        self.PLAYER_BOARD = [[" "] * self.BOARD_SIZE for _ in range(self.BOARD_SIZE)]
+        self.AI_BOARD = [[" "] * self.BOARD_SIZE for _ in range(self.BOARD_SIZE)]
+        self.PLAYER_GUESS_BOARD = [[" "] * self.BOARD_SIZE for _ in range(self.BOARD_SIZE)]
+        self.AI_GUESS_BOARD = [[" "] * self.BOARD_SIZE for _ in range(self.BOARD_SIZE)]
+        # Словарь, нужен для удобства ввода (так как итерация начинается от 0)
+        self.NUMBERS = {'1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5}
 
-    def print_board(self, board):
+        self.config = Config()
+
+    def print_board(self, board):  # board атрибут делает функцию нейтральной
+                                   # для вызова доски Игрока и ИИ
+        print("\nPlayer Board:")
         print("  1 2 3 4 5 6")
-        print("  +-+-+-+-+-+")
+        print("-+-++-+-+-+-+-+")
         row_number = 1
         for row in board:
-            print("%d|%s|" % (row_number, "|".join(row)))
+            print(f"{row_number}|{'|'.join(row)}|")
+            row_number += 1
+        if self.config.SHOW_ENEMY_BOARD == 1:
+            game.print_enemy_board()
+
+    def print_enemy_board(self):
+        print("\nEnemy Board:")
+        print("  1 2 3 4 5 6")
+        print("-+-++-+-+-+-+-+")
+        row_number = 1  # число для начала итерации строки
+        for row in self.AI_BOARD:
+            print(f"{row_number}|{'|'.join(row)}|")
             row_number += 1
 
     def place_ships(self, board):
         for ship_length in self.LENGTH_OF_SHIPS:
             while True:
-                if board == self.COMPUTER_BOARD:
+                if board == self.AI_BOARD:
                     orientation = random.choice(["H", "V"])
-                    row = random.randint(0, 5)
-                    column = random.randint(0, 5)
+                    row = random.randint(0, self.BOARD_SIZE - 1)
+                    column = random.randint(0, self.BOARD_SIZE - 1)
                     if self.check_ship_fit(ship_length, row, column, orientation):
-                        if not self.ship_overlaps(board, row, column, orientation, ship_length):
+                        if not self.ship_is_near(board, row, column, orientation, ship_length):
                             if orientation == "H":
                                 for i in range(column, column + ship_length):
                                     board[row][i] = "X"
@@ -36,9 +60,10 @@ class BattleshipGame:
                 else:
                     place_ship = True
                     print('Place the ship with a length of ' + str(ship_length))
+                    print('------------------')
                     row, column, orientation = self.user_input(place_ship)
                     if self.check_ship_fit(ship_length, row, column, orientation):
-                        if not self.ship_overlaps(board, row, column, orientation, ship_length):
+                        if not self.ship_is_near(board, row, column, orientation, ship_length):
                             if orientation == "H":
                                 for i in range(column, column + ship_length):
                                     board[row][i] = "X"
@@ -50,36 +75,50 @@ class BattleshipGame:
 
     def check_ship_fit(self, ship_length, row, column, orientation):
         if orientation == "H":
-            if column + ship_length > 6:
+            if column + ship_length > self.BOARD_SIZE:
                 return False
             else:
                 return True
         else:
-            if row + ship_length > 6:
+            if row + ship_length > self.BOARD_SIZE:
                 return False
             else:
                 return True
 
-    def ship_overlaps(self, board, row, column, orientation, ship_length):
+    def ship_is_near(self, board, row, column, orientation, ship_length):
+        # Проверяем, есть ли другие корабли рядом с текущим кораблем
+        # board - где проверяем, row/column - координаты корабля, orientation и ship_length - ориентация и длинна корабля
         if orientation == "H":
-            for i in range(column, column + ship_length):
-                if board[row][i] == "X":
-                    return True
-        else:
-            for i in range(row, row + ship_length):
-                if board[i][column] == "X":
-                    return True
-        return False
+            for i in range(max(0, row - 1), min(self.BOARD_SIZE, row + 2)):
+        # цикл для того, чтобы проверить три строки:
+        # одну выше текущей строки, текущую строку и одну ниже текущей строки.
+                for j in range(max(0, column - 1), min(self.BOARD_SIZE, column + ship_length + 1)):
+                    if board[i][j] == "X":
+                        if self.config.POS_FREE_MESSAGE == 1:
+                            print("Нельзя разместить корабль так близко друг к другу (H)")
+                        # проверка на 'X' в ячейке
+                        return True  # если есть, то True
+        else:  # для вертикали
+            for i in range(max(0, row - 1), min(self.BOARD_SIZE, row + ship_length + 1)):
+                for j in range(max(0, column - 1), min(self.BOARD_SIZE, column + 2)):
+                    if board[i][j] == "X":
+                        if self.config.POS_FREE_MESSAGE == 1:
+                            print("Нельзя разместить корабль так близко друг к другу (V)")
+                        return True
+        return False  # если пусто, False
 
     def user_input(self, place_ship):
+        orientation = None
         if place_ship:
-            while True:
-                try:
-                    orientation = input("Enter orientation (H or V): ").upper()
-                    if orientation == "H" or orientation == "V":
-                        break
-                except TypeError:
-                    print('Enter a valid orientation H or V')
+            if self.config.CHOOSE_ORIENTATION == 1:
+                print("PLAYER_INPUT = 1")
+                while True:
+                    try:
+                        orientation = input("Enter orientation (H or V): ").upper()
+                        if orientation == "H" or orientation == "V":
+                            break
+                    except TypeError:
+                        print('Enter a valid orientation H or V')
             while True:
                 try:
                     row = input("Enter the row 1-6 of the ship: ")
@@ -90,12 +129,12 @@ class BattleshipGame:
                     print('Enter a valid letter between 1-6')
             while True:
                 try:
-                    column = input("Enter the column of the ship: ").upper()
-                    if column in '12345':
-                        column = self.LETTERS_TO_NUMBERS[column]
+                    column = input("Enter the column 1-6 of the ship: ").upper()
+                    if column in '123456':
+                        column = self.NUMBERS[column]
                         break
-                except KeyError:
-                    print('Enter a valid letter between A-F')
+                except ValueError:
+                    print('Enter a valid letter between 1-6')
             return row, column, orientation
         else:
             while True:
@@ -108,12 +147,12 @@ class BattleshipGame:
                     print('Enter a valid letter between 1-6')
             while True:
                 try:
-                    column = input("Enter the column of the ship: ").upper()
-                    if column in 'ABCDEF':
-                        column = self.LETTERS_TO_NUMBERS[column]
+                    column = input("Enter the column 1-6 of the ship: ").upper()
+                    if column in '123456':
+                        column = self.NUMBERS[column]
                         break
-                except KeyError:
-                    print('Enter a valid letter between A-F')
+                except ValueError:
+                    print('Enter a valid letter between 1-6')
             return row, column
 
     def count_hit_ships(self, board):
@@ -126,17 +165,20 @@ class BattleshipGame:
 
     def turn(self, board):
         if board == self.PLAYER_GUESS_BOARD:
-            row, column = self.user_input(self.PLAYER_GUESS_BOARD)
+            self.config.CHOOSE_ORIENTATION = 0
+            print("PLAYER_INPUT = 0")
+            user_input = self.user_input(self.PLAYER_GUESS_BOARD)
+            row, column = user_input[0], user_input[1]  # [0] это строка [1] столбец
             if board[row][column] == "-":
                 self.turn(board)
             elif board[row][column] == "X":
                 self.turn(board)
-            elif self.COMPUTER_BOARD[row][column] == "X":
+            elif self.AI_BOARD[row][column] == "X":
                 board[row][column] = "X"
             else:
                 board[row][column] = "-"
         else:
-            row, column = random.randint(0, 5), random.randint(0, 5)
+            row, column = random.randint(0, self.BOARD_SIZE - 1), random.randint(0, self.BOARD_SIZE - 1)
             if board[row][column] == "-":
                 self.turn(board)
             elif board[row][column] == "X":
@@ -146,16 +188,17 @@ class BattleshipGame:
             else:
                 board[row][column] = "-"
 
+
 if __name__ == "__main__":
     print("\n======= Welcome to Battleship =======\n\n")
     game = BattleshipGame()
-    game.place_ships(game.COMPUTER_BOARD)
+    game.place_ships(game.AI_BOARD)
     game.print_board(game.PLAYER_BOARD)
     game.place_ships(game.PLAYER_BOARD)
 
     while True:
         while True:
-            print("\n -- Player Turn --\n")
+            print("\n -- Player Turn --")
             print('Guess enemy location\n')
             game.print_board(game.PLAYER_GUESS_BOARD)
             game.turn(game.PLAYER_GUESS_BOARD)
@@ -166,9 +209,9 @@ if __name__ == "__main__":
 
         while True:
             print("\n-- Computer Turn --\n")
-            game.turn(game.COMPUTER_GUESS_BOARD)
+            game.turn(game.AI_GUESS_BOARD)
             break
-        game.print_board(game.COMPUTER_GUESS_BOARD)
-        if game.count_hit_ships(game.COMPUTER_GUESS_BOARD) == 17:
+        game.print_board(game.AI_GUESS_BOARD)
+        if game.count_hit_ships(game.AI_GUESS_BOARD) == 17:
             print("Sorry, the computer won.")
             break
--- skillfactory/ship2/index2.py
+++ skillfactory/ship2/index2.py
@@ -3,20 +3,23 @@ import random
 class Config:
     def __init__(self):
         self.SHOW_ENEMY_BOARD = 0  # Отображение игрового поля противника
-        self.ai_ship_is_close_message = 1  # Нельзя разместить корабль так близко друг к другу
-        self.ai_shoot_coordinate_message = 1   # Координаты стрельбы ИИ
-        self.ai_ship_place_coordinate_message = 1  # Координаты расположения корабля
-        self.is_position_free_message = 1  # Свободно-ли место для корабля
+        self.POS_FREE_MESSAGE = 0  # Свободно-ли место возле корабля
+        self.CHOOSE_ORIENTATION = 1  # Выбор ориентации
+        self.SUM_ALL_SHIPS = 1  # Сумма всех кораблей
+
 
 class BattleshipGame:
     def __init__(self):
-        self.LENGTH_OF_SHIPS = [3, 3, 3, 3]
+        self.LENGTH_OF_SHIPS = [3, 4, 3]
         self.BOARD_SIZE = 6
         self.PLAYER_BOARD = [[" "] * self.BOARD_SIZE for _ in range(self.BOARD_SIZE)]
-        self.COMPUTER_BOARD = [[" "] * self.BOARD_SIZE for _ in range(self.BOARD_SIZE)]
+        self.AI_BOARD = [[" "] * self.BOARD_SIZE for _ in range(self.BOARD_SIZE)]
         self.PLAYER_GUESS_BOARD = [[" "] * self.BOARD_SIZE for _ in range(self.BOARD_SIZE)]
-        self.COMPUTER_GUESS_BOARD = [[" "] * self.BOARD_SIZE for _ in range(self.BOARD_SIZE)]
+        self.AI_GUESS_BOARD = [[" "] * self.BOARD_SIZE for _ in range(self.BOARD_SIZE)]
+        # Словарь, нужен для удобства ввода (так как итерация начинается от 0)
         self.NUMBERS = {'1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5}
+
+        self.config = Config()
 
     def print_board(self, board):  # board атрибут делает функцию нейтральной
                                    # для вызова доски Игрока и ИИ
@@ -25,28 +28,29 @@ class BattleshipGame:
         print("-+-++-+-+-+-+-+")
         row_number = 1
         for row in board:
-            print("%d|%s|" % (row_number, "|".join(row)))
+            print(f"{row_number}|{'|'.join(row)}|")
             row_number += 1
-        game.print_enemy_board()
+        if self.config.SHOW_ENEMY_BOARD == 1:
+            game.print_enemy_board()
 
     def print_enemy_board(self):
         print("\nEnemy Board:")
         print("  1 2 3 4 5 6")
         print("-+-++-+-+-+-+-+")
-        row_number = 1
-        for row in self.COMPUTER_BOARD:
-            print("%d|%s|" % (row_number, "|".join(row)))
+        row_number = 1  # число для начала итерации строки
+        for row in self.AI_BOARD:
+            print(f"{row_number}|{'|'.join(row)}|")
             row_number += 1
 
     def place_ships(self, board):
         for ship_length in self.LENGTH_OF_SHIPS:
             while True:
-                if board == self.COMPUTER_BOARD:
+                if board == self.AI_BOARD:
                     orientation = random.choice(["H", "V"])
                     row = random.randint(0, self.BOARD_SIZE - 1)
                     column = random.randint(0, self.BOARD_SIZE - 1)
                     if self.check_ship_fit(ship_length, row, column, orientation):
-                        if not self.ship_overlaps(board, row, column, orientation, ship_length):
+                        if not self.ship_is_near(board, row, column, orientation, ship_length):
                             if orientation == "H":
                                 for i in range(column, column + ship_length):
                                     board[row][i] = "X"
@@ -57,9 +61,10 @@ class BattleshipGame:
                 else:
                     place_ship = True
                     print('Place the ship with a length of ' + str(ship_length))
+                    print('------------------')
                     row, column, orientation = self.user_input(place_ship)
                     if self.check_ship_fit(ship_length, row, column, orientation):
-                        if not self.ship_overlaps(board, row, column, orientation, ship_length):
+                        if not self.ship_is_near(board, row, column, orientation, ship_length):
                             if orientation == "H":
                                 for i in range(column, column + ship_length):
                                     board[row][i] = "X"
@@ -71,45 +76,62 @@ class BattleshipGame:
 
     def check_ship_fit(self, ship_length, row, column, orientation):
         if orientation == "H":
+            # если длинна корабля и коорд столбца больше чем размер доски, то
             if column + ship_length > self.BOARD_SIZE:
-                return False
+                return False  # нельзя
             else:
-                return True
-        else:
+                return True  # можно
+        elif orientation == "V":
+            # тоже самое со строчкой (Вертикальная проверка) "V"
             if row + ship_length > self.BOARD_SIZE:
                 return False
             else:
                 return True
 
-    def ship_overlaps(self, board, row, column, orientation, ship_length):
+    def ship_is_near(self, board, row, column, orientation, ship_length):
         # Проверяем, есть ли другие корабли рядом с текущим кораблем
+        # board - где проверяем, row/column - координаты корабля, orientation и ship_length - ориентация и длинна корабля
         if orientation == "H":
             for i in range(max(0, row - 1), min(self.BOARD_SIZE, row + 2)):
+        # цикл для того, чтобы проверить три строки:
+        # одну выше текущей строки, текущую строку и одну ниже текущей строки.
                 for j in range(max(0, column - 1), min(self.BOARD_SIZE, column + ship_length + 1)):
                     if board[i][j] == "X":
-                        return True
-        else:
+                        if self.config.POS_FREE_MESSAGE == 1:
+                            print("Нельзя разместить корабль так близко друг к другу (H)")
+                        # проверка на 'X' в ячейке
+                        return True  # если есть, то True
+        else:  # для вертикали
             for i in range(max(0, row - 1), min(self.BOARD_SIZE, row + ship_length + 1)):
                 for j in range(max(0, column - 1), min(self.BOARD_SIZE, column + 2)):
                     if board[i][j] == "X":
+                        if self.config.POS_FREE_MESSAGE == 1:
+                            print("Нельзя разместить корабль так близко друг к другу (V)")
                         return True
-        return False
+        return False  # если пусто, False
 
     def user_input(self, place_ship):
+        orientation = None
         if place_ship:
-            while True:
-                try:
-                    orientation = input("Enter orientation (H or V): ").upper()
-                    if orientation == "H" or orientation == "V":
-                        break
-                except TypeError:
-                    print('Enter a valid orientation H or V')
+            if self.config.CHOOSE_ORIENTATION == 1:
+                print("PLAYER_INPUT = 1")
+                while True:
+                    try:
+                        orientation = input("Enter orientation (H or V): ").upper()
+                        if orientation == "H" or orientation == "V":
+                            break
+                        else:
+                            print("Empty cell")
+                    except TypeError:
+                        print('Enter a valid orientation H or V')
             while True:
                 try:
                     row = input("Enter the row 1-6 of the ship: ")
                     if row in '123456':
                         row = int(row) - 1
                         break
+                    else:
+                        print("Empty cell")
                 except ValueError:
                     print('Enter a valid letter between 1-6')
             while True:
@@ -118,6 +140,8 @@ class BattleshipGame:
                     if column in '123456':
                         column = self.NUMBERS[column]
                         break
+                    else:
+                        print("Empty cell")
                 except ValueError:
                     print('Enter a valid letter between 1-6')
             return row, column, orientation
@@ -128,6 +152,8 @@ class BattleshipGame:
                     if row in '123456':
                         row = int(row) - 1
                         break
+                    else:
+                        print("Empty cell")
                 except ValueError:
                     print('Enter a valid letter between 1-6')
             while True:
@@ -136,9 +162,14 @@ class BattleshipGame:
                     if column in '123456':
                         column = self.NUMBERS[column]
                         break
+                    else:
+                        print("Empty cell")
                 except ValueError:
                     print('Enter a valid letter between 1-6')
             return row, column
+
+    def calculate_total_ship_length(self):
+        return sum(self.LENGTH_OF_SHIPS)
 
     def count_hit_ships(self, board):
         count = 0
@@ -150,13 +181,15 @@ class BattleshipGame:
 
     def turn(self, board):
         if board == self.PLAYER_GUESS_BOARD:
+            self.config.CHOOSE_ORIENTATION = 0
+            print("PLAYER_INPUT = 0")
             user_input = self.user_input(self.PLAYER_GUESS_BOARD)
-            row, column = user_input[0], user_input[1]
+            row, column = user_input[0], user_input[1]  # [0] это строка [1] столбец
             if board[row][column] == "-":
                 self.turn(board)
             elif board[row][column] == "X":
                 self.turn(board)
-            elif self.COMPUTER_BOARD[row][column] == "X":
+            elif self.AI_BOARD[row][column] == "X":
                 board[row][column] = "X"
             else:
                 board[row][column] = "-"
@@ -175,26 +208,26 @@ class BattleshipGame:
 if __name__ == "__main__":
     print("\n======= Welcome to Battleship =======\n\n")
     game = BattleshipGame()
-    game.place_ships(game.COMPUTER_BOARD)
+    game.place_ships(game.AI_BOARD)
     game.print_board(game.PLAYER_BOARD)
     game.place_ships(game.PLAYER_BOARD)
 
     while True:
         while True:
-            print("\n -- Player Turn --\n")
-            print('Guess enemy location\n')
+            print("\n -- Player Turn --")
+            print('Guess enemy location')
             game.print_board(game.PLAYER_GUESS_BOARD)
             game.turn(game.PLAYER_GUESS_BOARD)
             break
-        if game.count_hit_ships(game.PLAYER_GUESS_BOARD) == 17:
+        if game.count_hit_ships(game.PLAYER_GUESS_BOARD) == game.calculate_total_ship_length():
             print("You win!")
             break
 
         while True:
             print("\n-- Computer Turn --\n")
-            game.turn(game.COMPUTER_GUESS_BOARD)
+            game.turn(game.AI_GUESS_BOARD)
             break
-        game.print_board(game.COMPUTER_GUESS_BOARD)
-        if game.count_hit_ships(game.COMPUTER_GUESS_BOARD) == 17:
+        game.print_board(game.AI_GUESS_BOARD)
+        if game.count_hit_ships(game.AI_GUESS_BOARD) == game.calculate_total_ship_length():
             print("Sorry, the computer won.")
             break
